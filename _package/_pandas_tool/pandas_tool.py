cac231b0db4672dac13bbc979e07ee92376987c5
unknown <forclass021117@gmail.com>
2020-10-24 23:22:16 +0800

    refine panda tool

--- _package/_pandas_tool/pandas_tool.py
+++ _package/_pandas_tool/pandas_tool.py
@@ -1,6 +1,12 @@
 import random
 import pandas as pd
 import numpy as np
+import time
+from datetime import datetime
+
+import sys
+sys.path.append('../..') # 添加相對路徑上層到sys.path，讓程式找到的模組_package
+from _package._random_tool.random_tool import rand_time
 
 def to_csv(df, file_name):
     df.to_csv(file_name, index=False)
@@ -39,8 +45,115 @@ def rand_df(rand_dict, row_num, miss_rate=0, na_values = None):
         df[k] = [miss_judge(v()) for _ in range(row_num)]
     return df
 
+def df_groupby(df, col, inplace = False):
+    """
+    函數功能: 
+    將dataframe依指定的col group起來，
+    回傳一個字典
+    參數:
+        inplace: 是否原地修改df
+    caller example:
+    >>> df = pd.DataFrame({'Animal': ['Falcon', 'Falcon', 'Parrot', 'Parrot'],
+                       'Max Speed': [380., 370., 24., 26.],
+                       'Value':[3,3,2,6]})
+    >>> print(df_groupby(df, 'Animal'))
+    {'Falcon':    Animal  Max Speed  Value
+                0  Falcon      380.0      3
+                1  Falcon      370.0      3,
+     'Parrot':    Animal  Max Speed  Value
+                2  Parrot       24.0      2
+                3  Parrot       26.0      6}
+    """
+    if not inplace:
+        df = df.copy()
+    return {name:group for name, group in df.groupby([col])}
+    
+def df_groupby_list(df, col, inplace = False, remove_dup = True):
+    """
+    函數功能: 
+    將dataframe依指定的col group起來，
+    其它欄位會group成list
+    參數:
+        inplace: 是否原地修改df
+        remove_dup: 是否去除重複值
+    caller example:
+    >>> df = pd.DataFrame({'Animal': ['Falcon', 'Falcon', 'Parrot', 'Parrot'],
+                       'Max Speed': [380., 370., 24., 26.],
+                       'Value':[3,3,2,6]})
+    >>> print(df_groupby_list(df, 'Animal'))
+    
+                 Max Speed   Value
+    Animal                        
+    Falcon  [370.0, 380.0]     [3]
+    Parrot    [24.0, 26.0]  [2, 6]
+    """
+    if not inplace:
+        df = df.copy()
+    other_cols = list(filter(lambda x:x!=col, df.columns))
+    df[other_cols] = df[other_cols].applymap(lambda x:[x])
+    df = df.groupby([col]).sum()
+    if remove_dup:
+        df[other_cols] = df[other_cols].applymap(lambda x:list(set(x)))
+    return df
+
+def df_range_filter(df, *args):
+    """
+    函數功能: 
+    將dataframe依指定條件過濾
+    參數:
+        *args 是長度為3的tuple，格式如:
+        ('欄位名', 比較運算符, value or string)
+       
+    caller example:
+    >>> df = pd.DataFrame({'Animal': ['Falcon', 'Falcon', 'Parrot', 'Parrot'],
+                           'Max Speed': [380., 370., 24., 26.],
+                           'Value':[3,3,2,6]})
+    >>> print(df_range_filter(df, ('Max Speed', '>', 375)))
+    
+       Animal  Max Speed  Value
+    2  Parrot       24.0      2
+    """
+    df = df.copy() #不修改原殆df
+    for arg in args:
+        assert len(arg)==3 and arg[1] in ['==','<=','>=','<','>','!='], 'wrong format of conditions.'
+        val = "'"+arg[2]+"'" if type(arg[2])==str else arg[2]
+        exec(f"m = df['{arg[0]}']{arg[1]}{val}")
+        mask = locals()['m'] # 將exec裡的區域變數取出
+        df = df[mask]
+    return df
+
+def time_diff(series_1, series_2):
+    """
+    函數功能: 
+    回傳兩個series的時間差，單位為秒(其中一個變數可以是常量)
+    """
+    arr = [series_1, series_2]
+    for i, e in enumerate(arr):
+        try:
+            arr[i] = e.apply(lambda x: int(time.mktime(pd.Timestamp(x).timetuple())))
+        except:
+            arr[i] = int(time.mktime(pd.Timestamp(e).timetuple()))
+    return arr[0] - arr[1]    
+    
         
 if __name__=='__main__':
     rand_dict = {'status': lambda: random.choice(['Wait', 'Start'])}
-    print(rand_df(rand_dict, 20, miss_rate=0.2, na_values=['None', 'NoData']))
+    random_df = rand_df(rand_dict, 20, miss_rate=0.2, na_values=['None', 'NoData'])
+    
+    df = pd.DataFrame({'Animal': ['Falcon', 'Falcon', 'Parrot', 'Parrot'],
+                                  'Max Speed': [380., 370., 24., 26.],
+                                  'Value':[3,3,2,6]})
+    print(df_groupby_list(df, 'Animal'))
+    print(df_groupby(df, 'Animal'))
+    print(df_range_filter(df, ('Value', '==', 2)))
+    
+    
+    min_time = (2020,10,23,0,0,0)
+    max_time = (2020,10,23,2,0,0)
+    now_time = (2020,10,23,4,0,0)
+    rand_dict = {'time_1': lambda: rand_time(min_time, max_time),
+                 'time_2': lambda: rand_time(min_time, max_time)}
+    df = rand_df(rand_dict, 20)
+    df['time_diff_minutes'] =  time_diff(df['time_1'], df['time_2'])//60
+    df['time_diff_now_minutes'] =  time_diff(datetime(*now_time), df['time_2'])//60
     
